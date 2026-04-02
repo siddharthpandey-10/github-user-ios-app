168062e5c1da53a85b2ee0460f90eb31f34822cb
Siddharth <officialsiddharth.pandey@gmail.com>
2026-04-03 04:39:15 +0530

    Initial Commit

---
+++ GitHubUserApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubUserApp
+//
+//  Created by Siddharth Pandey on 03/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubUserApp/GitHubUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserAppApp.swift
+//  GitHubUserApp
+//
+//  Created by Siddharth Pandey on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
